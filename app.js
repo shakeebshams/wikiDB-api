e1382dcd430a15717e17bb6c5f040c180c809524
shakeebshams <shakeebshams@gmail.com>
2020-04-27 01:45:37 -0400

    added delete all route to api

--- app.js
+++ app.js
@@ -50,6 +50,16 @@ app.post('/articles', function(req, res) {
     });
 });
 
+app.delete('/articles', function(req, res) {
+    Article.deleteMany(function(err) {
+        if (err) {
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
 
 
 
